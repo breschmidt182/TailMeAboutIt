974ddd50bc2b3b9ba072cb66573378ac0596f4f3
Leandri Beavis <leandrikuyk@gmail.com>
2022-12-11 17:54:54 -0500

    imported pages and added routing for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import Home from './pages/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import NoMatch from './pages/NoMatch';
+import Toys from './pages/Toys';
+import Accessories from './pages/Accessories';
+import Boxes from './pages/Boxes';
+import Sale from './pages/Sale';
+//import Login from './pages/Login';
+//import Profile from './pages/Profile';
+//import Signup from './pages/Signup';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route
+                path="/"
+                element={<Home />}
+              />
+              <Route
+                path="/toys"
+                element={<Toys />}
+              />
+              <Route
+                path="/accessories"
+                element={<Accessories />}
+              />
+              <Route
+                path="/boxes"
+                element={<Boxes />}
+              />
+              <Route
+                path="/sale"
+                element={<Sale />}
+              />
+              {/* <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              /> */}
+              <Route
+                path="/nomatch"
+                element={<NoMatch/>}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
   );
 }
 

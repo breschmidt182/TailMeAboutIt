ea80e3bd353c095ae81277e33e2bd03f0aebcb70
Leandri Beavis <leandrikuyk@gmail.com>
2022-12-11 17:41:40 -0500

    added Header and Footer components

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header className="bg-secondary mb-4 py-2 flex-row align-center">
+            <div className="container flex-row justify-space-between-lg justify-center align-center">
+                <Link to="/">
+                    <h1>Tail Me About It</h1>
+                </Link>
+                {/* TODO: Login and Sign Up page? */}
+                <nav className="text-center">
+                    <Link to="/toys">Toys</Link>
+                    <Link to="/accessories">Accessories</Link>
+                    <Link to="/boxes">Holiday Boxes</Link>
+                    <Link to="/sale">On Sale</Link>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;

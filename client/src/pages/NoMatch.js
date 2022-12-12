0f5da392e2daa23240d729f828c38ac29eab921d
Leandri Beavis <leandrikuyk@gmail.com>
2022-12-12 16:54:05 -0500

    create NoMatch (error) page template

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div>
+            Oops, we couldn't find the page.
+        </div>
+    )
+}
+
+export default NoMatch;

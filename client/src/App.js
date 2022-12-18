2f618027d1840db8ce7eb53b754835aae1f99a31
Leandri Beavis <leandrikuyk@gmail.com>
2022-12-18 17:32:15 -0500

    added home page and imported to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+
+    return (
+        <main>
+            <div className='flex-row justify-space-between'>
+                <div className='col-12 mb-3'>
+                    <div>Loading...</div>
+                </div>
+            </div>
+        </main>
+    );
+};
+
+export default Home;
